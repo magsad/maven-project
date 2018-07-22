bf0d2b2852eb4d6a2c79e54cca5f2e187611781f
Magdalena Sadowska <magdalena.sadowska@capgemini.com>
2018-07-22 21:46:22 +0100

    Fix warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
 /**
  * This is a main method.
  */
-  public final String greet(final String someone) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
